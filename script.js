791902babf3e50121e0baf634de7c1cfbbd30c3c
Sayan Chakraborty <sayanchakraborty710@gmail.com>
2024-02-03 00:35:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// load the header and footer into all  pages
+$(function(){
+    $("#head").load('header.html')
+    $("#foot").load('footer.html')
+})
+
---
+++ swip.js
@@ -0,0 +1,30 @@
+// Creation of slide functionality
+var swiper = new Swiper(".slide-container", {
+    slidesPerView: 4,
+    spaceBetween: 20,
+    sliderPerGroup: 4,
+    loop: true,
+    centerSlide: "true",
+    fade: "true",
+    grabCursor: "true",
+    // For nav button functionality
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    // For making the slider responsive
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
